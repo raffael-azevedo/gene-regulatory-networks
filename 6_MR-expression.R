36ecc23dc6018ed36049f49b85765b347bf062e2
Raffael Azevedo <raffael.azevedo@gmail.com>
2019-04-04 16:55:43 -0300

    Minor changes: fixed plot of comparisons

--- 6_MR-expression.R
+++ 6_MR-expression.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(EnvStats)
+library(ggpubr)
 load("/home/raffael/GDrive/Doutorado/Sepsis R/Paper/SIGNATURE_GSE26378/validation/commonRegulons.RData")
 regulons <- commonGenes[intersection]
 regulons <- c("MEF2A", "RFX2", "TRIM25", "GATA3", "HOXB2", "KLF12", "NR3C2", "RORA", "ZKSCAN8", "ZNF134", "ZNF234", "ZNF235", "ZNF329", "ZNF331", "ZNF529")
@@ -12,14 +13,14 @@ data <- ReadAffy()
 exp <- rma(data)
 GSE4607 <- exprs(exp)
 #names_index <- setNames(c(rep("control", 15), rep("shock_ns", 9), rep("shock_s", 33)),colnames(GSE4607))
-names_index <- setNames(c(rep("control", 15), rep("shock", 42)),colnames(GSE4607))
+names_index <- setNames(c(rep("control", 15), rep("sepsis", 42)),colnames(GSE4607))
 #colnames(GSE4607) <- c(rep("control", 15), rep("shock_ns", 9), rep("shock_s", 33))
 a <- as.data.frame(rowMeans(GSE4607[,1:15]))
 b <- as.data.frame(rowMeans(GSE4607[,16:57]))
 # b <- as.data.frame(rowMeans(GSE4607[,16:24]))
 # c <- as.data.frame(rowMeans(GSE4607[,25:57]))
 dataset <- cbind(a,b)
-colnames(dataset) <- c("control", "shock")
+colnames(dataset) <- c("control", "sepsis")
 load("/home/raffael/GDrive/Doutorado/Sepsis R/Paper/SIGNATURE_GSE26378/GSE4607/RData/chunk1_nrData_GSE4607.RData")
 PROBEID <- rownames(dataset)
 rownames(dataset) <- NULL
@@ -53,8 +54,13 @@ for(gene in intersection){
 }
 inter <- subset(best_probes, SYMBOL%in%intersection)
 inter$SYMBOL <- factor(inter$SYMBOL, levels = regulons)
-ggplot(inter) + 
-  geom_boxplot(aes(x = tipo, y = value, fill = tipo)) + 
+
+comparison <- compare_means(formula = value ~ tipo, group.by = "SYMBOL", data = inter, p.adjust.method = "fdr", method = "wilcox.test", paired = F)
+comparison <- inter %>% group_by(SYMBOL) %>% summarise(y.position = max(value) + 1) %>% inner_join(comparison)
+
+ggboxplot(inter,x = "tipo", y = "value", fill = "tipo",palette =c("#00AFBB", "#FC4E07")) +
   facet_wrap(SYMBOL~., nrow = 3,ncol = 5) +
-  xlab("Clinical Type") + ylab("Arbitrary Expression Units (AEUs)") + ggtitle("Master regulators expression in GSE4607") +
-  stat_compare_means(paired = F, aes(x = tipo, y = value),p.adjust.methods="fdr")
+  ylim(c(0,12.5)) +
+  stat_pvalue_manual(comparison, label = "p.adj") +
+  theme_grey() +
+  xlab("Clinical Type") + ylab("Arbitrary Expression Units (AEUs)") + ggtitle("Master regulators expression in GSE4607") + guides(fill=guide_legend(title="Groups"))
